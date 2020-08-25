e559fd0b2a93aedf349c43611c340cdadd3f4f68
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-25 16:52:00 -0400

    updated with notes commented out, testing examples

---
+++ script.js
@@ -0,0 +1,8 @@
+// find a way to link name from customers to the html file
+// will not need to code each item in text for html, should be able to build one 'customer' and have the js populate each field
+// format looks like:
+// picture
+// email
+// street address
+// DOB
+// customer since
