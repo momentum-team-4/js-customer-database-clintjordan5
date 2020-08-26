8ceb2898ac98f66c7c667ad3bebef4f7965681c1
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-25 22:39:40 -0400

    fixed console errors. added moment.js, attempting to show database info on page.

--- script.js
+++ script.js
@@ -6,3 +6,41 @@
 // street address
 // DOB
 // customer since
+
+function renderPageContent () {
+    let ulElement = document.querySelector('#people')
+    for (let person of people) {
+        let listElement = document.createElement ('li')
+        let imageElement = createImageElement(itemg.imgUrl)
+        listElement.appendChild(imageElement)
+        console.log(listElement)
+        ulElement,appendChild(listElement)
+    }
+}
+
+function createImageElement (url) {
+    let imageElement = document.createElement('img')
+    imageElement.src = url
+    return imageElement
+}
+
+// renderPageContent()
+
+const people = [
+    {
+        title: 'Curtis Ryan' ,
+        imgUrl: 'https://randomuser.me/api/portraits/med/men/0.jpg'
+    },
+    {
+        title: 'Martin Terry' ,
+        imgUrl: 'https://randomuser.me/api/portraits/med/men/6.jpg'
+    },
+    {
+        title: 'Christine Cooper' ,
+        imgUrl: 'https://randomuser.me/api/portraits/med/women/52.jpg'
+    }
+]
+
+// started making list of people and using the "medium" image, but stuck as to whether to continue writing this out.
+// currently the only way to display content on page is to manually type the data in html. 
+// positive note - no errors in console
